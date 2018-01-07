761d3112de38d1f1af8243d6c77de97be240c437
jack <616651001@qq.com>
2018-01-07 22:11:50 +0800

    添加插件

---
+++ B5-jQuery-事件与插件/案例/jquery-MrLv-1.11.1-min.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Lenovo on 2016/9/17.
+ */
+
+//绑定到原型上,调用者是jquery对象
+$.fn.setColorRed = function () {
+    this.css({"color":"red"});
+}
+
+
+//绑定到$上，调用者是$
+$.setColorRed = function (e) {
+    e.css({"color":"red"});
+}
