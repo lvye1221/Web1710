15d9ecfcb504d291cf2648327ffb3e75b4328bc9
杨杰 <616651001@qq.com>
2018-01-18 13:57:20 +0800

    实现自己的飞机拖拽

---
+++ C4-面向对象-飞机大战/js/GameEngine.js
@@ -0,0 +1,35 @@
+
+
+// 游戏引擎        游戏背景						开始， 结束
+
+
+//  仅此1份的对象，我们一般采用 字面量的形式  会更好
+
+// 游戏引擎
+var gameEngine = {
+	
+	ele: $("#box"),		// 属性： 找到背景
+	
+	myPlane: null, 	 	// 自己的飞机
+	
+	start: function() {   // 方法: 启动游戏
+		
+		// 1. 游戏背景动起来
+		this.moveBg()
+		
+		// 2. 创建自己的飞机
+		this.myPlane = new MyPlane()
+	}, 
+	
+	
+	moveBg: function() {	// 方法1： 滚动背景
+		// 定时移动背景图片: 改变定位
+		var self = this
+		this.ele.animate({"background-position-y": "-230%"}, 10000, "linear", function() {
+			// 先让图片回到 100% 的位置
+			self.ele.css({"background-position-y": "0%"})
+			// 继续滚动
+			self.moveBg()
+		})
+	}
+}
---
+++ C4-面向对象-飞机大战/js/MyPlane.js
@@ -0,0 +1,30 @@
+
+function MyPlane() {	// 构造函数
+
+	this.ele = $("<div id='myPlane'></div>")	 //创建飞机物体
+	this.ele.appendTo( "#box" )  // 添加到游戏盒子中
+	
+	DragBox.call(this, "#myPlane")  // 继承属性： 拖拽物体，属性
+}
+
+MyPlane.prototype = new DragBox()  // 继承方法
+
+MyPlane.prototype.move = function(x, y) {
+	if (x < 0) {
+		x = 0
+	}
+	
+	// 最大x，    页面宽度 - 飞机自身的宽度
+	var maxX = $("body").width() - this.ele.width()
+	if (x > maxX) {
+		x = maxX
+	}
+	
+	if (y < 0) {
+		y = 0
+	}
+	
+	// 调用父类的方法，移动当前元素
+	DragBox.prototype.move.call(this, x, y)
+}
+
